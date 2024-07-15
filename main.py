54c65cd56fee5f1e5c987454bd3a6216fdbc7fa9
Oukil Ilias <oukililias@gmail.com>
2024-07-15 14:55:21 +0100

    asassa

--- main.py
+++ main.py
@@ -53,18 +53,21 @@ async def send_message():
 async def send_discord_message(channel_id, novel_title, chapter_number, chapter_title, chapter_id, free_chapter_number, free_chapter_title, free_chapter_id, novel_id, cover_id):
     channel = bot.get_channel(channel_id)
     if channel:
-        # Define novel_id to role_id mapping
+        # Define novel_id to role_id and color mapping
         role_map = {
-            '827757d8-161c-441b-8240-f4dbd7407ce4': 1259695280413085737,
-            '69d6ab1d-4da5-4e84-8ffb-190308540908': 1259695317436076082
+            '827757d8-161c-441b-8240-f4dbd7407ce4': {'role_id': 1259695280413085737, 'color': discord.Color.blue()},
+            '69d6ab1d-4da5-4e84-8ffb-190308540908': {'role_id': 1259695317436076082, 'color': discord.Color.red()}
         }
         
-        role_id = role_map.get(novel_id)
+        role_info = role_map.get(novel_id, {})
+        role_id = role_info.get('role_id')
+        embed_color = role_info.get('color', discord.Color.default())
+        
         role = None
         if role_id:
             role = channel.guild.get_role(role_id)
         
-        embed = discord.Embed(color=discord.Color.blue())
+        embed = discord.Embed(color=embed_color)
         
         embed.title = novel_title
         embed.description = f"Premium Chapter:\n[{chapter_number} - {chapter_title}](https://genesistudio.com/viewer/{chapter_id})\n\n Free Chapter:\n[{free_chapter_number} - {free_chapter_title}](https://genesistudio.com/viewer/{free_chapter_id})"
